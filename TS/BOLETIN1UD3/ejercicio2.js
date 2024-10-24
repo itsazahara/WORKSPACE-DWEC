f46cccf575b42be20967ff6bb18d1bc0edb85085
itsazahara <aromsch1403@g.educaand.es>
2024-10-24 10:24:08 +0200

    modificaciones

--- TS/BOLETIN1UD3/ejercicio2.js
+++ TS/BOLETIN1UD3/ejercicio2.js
@@ -1,10 +1,5 @@
-var expReg = /[0-9]/;
-var expReg2 = new RegExp("[0-9]");
-console.log("Prueba 1 " + expReg.test("Hola"));
-console.log("Prueba 2 " + expReg2.test("Hola"));
-console.log("Prueba 3 " + expReg.test("1"));
-console.log("Prueba 4 " + expReg2.test("1"));
-console.log("Prueba 5 " + expReg.test("123"));
-console.log("Prueba 6 " + expReg2.test("123"));
-console.log("Prueba 7 " + expReg.test("abc123"));
-console.log("Prueba 8 " + expReg2.test("abc123"));
+function validarDatos() {
+    var expRegMail = new RegExp("[0-9][a-z][A-Z]");
+    var mail = document.getElementById("mail");
+    alert(mail.value);
+}
