ca703df202c05e2c4f5e256a8a4994da53f3690f
itsazahara <aromsch1403@g.educaand.es>
2024-10-31 09:53:14 +0100

    Modificacion en la actividad 3

--- TS/BOLETIN2UD3/ejercicio3.ts
+++ TS/BOLETIN2UD3/ejercicio3.ts
@@ -1,4 +1,11 @@
+const http_prefix = "https://";
+
 function solicita_redireccion(){
     var url: string = prompt("Introduce la url") as string;
+
+    if(url.indexOf(http_prefix) == -1){
+        url = http_prefix + url;
+    }
+
     window.location.assign(url);
 }
