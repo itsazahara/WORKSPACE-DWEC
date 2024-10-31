9f8547aa57b6086a73a33255802cbd51a2e4d77c
itsazahara <aromsch1403@g.educaand.es>
2024-10-31 09:20:30 +0100

    Subida del ejercicio2

--- TS/BOLETIN2UD3/ejercicio2.ts
+++ TS/BOLETIN2UD3/ejercicio2.ts
@@ -1,13 +1,6 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const botonDimension = document.getElementById("botonDimension");
-
-    if (botonDimension) {
-        botonDimension.addEventListener("click", () => {
-            const anchoVentana = window.innerWidth;
-            const altoVentana = window.innerHeight;
-
-            console.log(`Ancho de la ventana: ${anchoVentana}px`);
-            console.log(`Altura de la ventana: ${altoVentana}px`);
-        });
-    }
-});
+function mostrar_altura() {
+    console.log("La altura de la ventana es: " + window.innerHeight);
+}
+function mostrar_anchura() {
+    console.log("La anchura de la ventana es: " + window.innerWidth);
+}
