634541c56825dcff8a27dd163659d3404c0d04b9
itsazahara <aromsch1403@g.educaand.es>
2024-10-31 09:40:47 +0100

    Subida del ejercicio 3

--- TS/BOLETIN2UD3/ejercicio3.ts
+++ TS/BOLETIN2UD3/ejercicio3.ts
@@ -1,17 +1,4 @@
-window.addEventListener("DOMContentLoaded", () => {
-    const botonRedireccion = document.getElementById("botonRedireccion");
-
-    if (botonRedireccion) {
-        botonRedireccion.addEventListener("click", () => {
-            const urlDestino = prompt("Ingresa la URL a la que deseas ir (por ejemplo, https://www.ejemplo.com):");
-
-            if (urlDestino && urlDestino.startsWith("http")) {
-                window.location.href = urlDestino;
-            } else {
-                console.log("URL no válida o acción cancelada.");
-            }
-        });
-    }
-});
-
-//NO TERMINADO POR ERROR
+function solicita_redireccion(){
+    var url: string = prompt("Introduce la url") as string;
+    window.location.assign(url);
+}
