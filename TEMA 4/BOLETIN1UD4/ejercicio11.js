0b607d37c0a3a9d819f114ce5c1631cf5104d34e
itsazahara <aromsch1403@g.educaand.es>
2024-12-03 11:11:48 +0100

    Boletin actualizado

---
+++ TEMA 4/BOLETIN1UD4/ejercicio11.js
@@ -0,0 +1,10 @@
+let dias = new Set();
+dias.add("Lunes");
+dias.add("Martes");
+dias.add("Miércoles");
+dias.add("Jueves");
+dias.add("Viernes");
+dias.add("Sábado");
+dias.add("Domingo");
+
+console.log(dias);
--- TEMA 4/BOLETIN1UD4/ejercicio7.js
+++ TEMA 4/BOLETIN1UD4/ejercicio7.js
@@ -1,3 +1,20 @@
+const TAM = 10;
+let matriz1 = new Array(TAM);
+let matriz2 = new Array(TAM);
+
+//Inicializamos ambas matrices a numeros aleatorios.
+for (let i = 0; i < TAM; i++) {
+    let array1 = new Array(TAM);
+    let array2 = new Array(TAM);
+    for (let j = 0; j < TAM; j++) {
+        array1[j] = Math.round(Math.random() * 9);
+        array2[j] = Math.round(Math.random() * 9);
+    }
+    matriz1[i] = array1;
+    matriz2[i] = array2;
+}
+
+console.log(matriz1);
 console.log(matriz2);
 
 function sumar_matrices() {
@@ -5,10 +22,23 @@ function sumar_matrices() {
     for (let i = 0; i < TAM; i++) {
         let arrayResult = new Array(TAM);
         for (let j = 0; j < TAM; j++) {
-            arrayResult[j] = matriz[i][j] + matriz2[i][j];
+            arrayResult[j] = matriz1[i][j] + matriz2[i][j];
         }
         matrizResult[i] = arrayResult;
     }
 
     console.log(matrizResult);
+}
+
+function traspuesta() {
+    let traspuesta = new Array(TAM);
+    for (let i = 0; i < TAM; i++) {
+        let arrayTraspuesta = new Array(TAM);
+        for (let j = 0; j < TAM; j++) {
+            arrayTraspuesta[j] = matriz1[j][i];
+        }
+        traspuesta[i] = arrayTraspuesta;
+    }
+
+    console.log(traspuesta);
 }
